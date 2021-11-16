7484ca39815813fd204fa96dba748b7efd0d9267
Daohuy <huyhalo20@gmail.com>
2021-11-17 00:36:50 +0700

    hello

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'HELUU!',
+    introDesc: ` Tớ có điều muốn nói với cậu rất lâu rồi, ấn vào bên dưới để xem nhé`,
+    btnIntro: 'Ấn đi tình yêu',
+    title: 'Phải chăng em đã nợ anh tiền mà em không nhớ? 😙',
+    desc: 'Phải chăng em muốn trả anh ngay bây giờ  ',
+    btnYes: 'Dạ đúng rồi ạ !!',
+    btnNo: 'Bị mơ ngủ à ??',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại không trả tiền anh',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì đẹp của anh làm em ngây ngất',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Còn đợi gì mà không nhắn tin cho anh :v.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/wenlang.anhduong/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
